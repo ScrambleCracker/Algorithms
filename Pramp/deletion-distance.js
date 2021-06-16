8d1e80f204dc28788a08e95fc51b7ed2f21ec32e
Oleh Horitsyn <oleg.goritsyn@gmail.com>
2021-06-16 13:45:55 +0300

    add Pramp deletion distance recursive solution

--- Pramp/deletion-distance.js
+++ Pramp/deletion-distance.js
@@ -20,8 +20,27 @@ function deletionDistance(str1, str2) {
     return prevMemo[m];
 }
 
+
+function deletionDistanceRecursive(str1, str2) {
+    const [n, m] = [str1.length, str2.length];
+    if (n === 0 || m === 0) return Math.max(n, m);
+
+    return str1[0] === str2[0]
+        ? deletionDistanceRecursive(str1.substring(1), str2.substring(1))
+        : 1 + Math.min(
+            deletionDistanceRecursive(str1.substring(1), str2),
+            deletionDistanceRecursive(str1, str2.substring(1))
+        );
+}
+
 console.assert(3 === deletionDistance('hit', 'heat'));
 console.assert(0 === deletionDistance('some', 'some'));
 console.assert(9 === deletionDistance('some', 'thing'));
 console.assert(5 === deletionDistance('', 'thing'));
 console.assert(0 === deletionDistance('', ''));
+
+console.assert(3 === deletionDistanceRecursive('hit', 'heat'));
+console.assert(0 === deletionDistanceRecursive('some', 'some'));
+console.assert(9 === deletionDistanceRecursive('some', 'thing'));
+console.assert(5 === deletionDistanceRecursive('', 'thing'));
+console.assert(0 === deletionDistanceRecursive('', ''));
