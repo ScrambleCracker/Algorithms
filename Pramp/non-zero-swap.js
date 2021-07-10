2eaea05c49634b9f6cbff83720495c7553bd9c31
Oleh Horitsyn <oleg.goritsyn@gmail.com>
2021-07-10 13:40:44 +0300

    improve moveZerosToEnd solution

--- Pramp/non-zero-swap.js
+++ Pramp/non-zero-swap.js
@@ -1,31 +1,26 @@
+/**
+ @param arr: integer[]
+ @return: integer[]
+ */
 function moveZerosToEnd(arr) {
-    /**
-     @param arr: integer[]
-     @return: integer[]
-     */
-
-        // your code goes here
-
-        // [0, 1, 0, 0, 3] -> [1, 0 , 0, 0, 3] -> [1, 3, 0, 0, 0]
-        // [1, 2, 3, 0, 4] -> [1, 2, 3, 4, 0]
-        // [0, 3, 4, 0, 0, 3, 0, 1] -> [3, 0, 4, ...]
-        // -> [3, 4, 0, 0, 0, 3, 0, 1] ... -> [3, 4, 3, 1, 0...] O(n^2)
-        // nonZero = 0; curr = 0;
-        // [3, 3, 4...] -> [3, 4, 4,...] O(n) O(1)
+    // [0, 1, 0, 0, 3] -> [1, 0 , 0, 0, 3] -> [1, 3, 0, 0, 0]
+    // [1, 2, 3, 0, 4] -> [1, 2, 3, 4, 0]
+    // [0, 3, 4, 0, 0, 3, 0, 1] -> [3, 0, 4, ...]
+    // -> [3, 4, 0, 0, 0, 3, 0, 1] ... -> [3, 4, 3, 1, 0...] O(n^2)
+    // nonZero = 0; curr = 0;
+    // [0, 3, 4, 0, 0, 3, 0, 1] -> [3, 0, 4...] -> [3, 4, 0,...] O(n) O(1)
 
     let nonZero = 0;
 
     for (let idx = 0; idx < arr.length; idx++) {
         if (arr[idx] !== 0) {
             arr[nonZero++] = arr[idx];
+            arr[idx] = 0;
         }
     }
 
-    while (nonZero < arr.length) {
-        arr[nonZero++] = 0;
-    }
     return arr;
 }
 
-const input = [];
+const input = [0, 3, 4, 0, 0, 3, 0, 1];
 console.log(moveZerosToEnd(input));
