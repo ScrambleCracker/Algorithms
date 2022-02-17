6948bec041b1cabaac616b67558b0ea360a7540d
Oleh Horitsyn <oleg.goritsyn@gmail.com>
2022-02-17 18:44:08 +0200

    add currt with placeholder implementation

--- Frontend/curry.js
+++ Frontend/curry.js
@@ -2,10 +2,24 @@
  * @param { (...args: any[]) => any } fn
  * @returns { (...args: any[]) => any }
  */
-function curry(fn) {
+export function curry(fn) {
     return function curried(...args) {
         return args.length >= fn.length
             ? fn.apply(this, args)
             : curried.bind(this, ...args);
     };
 }
+
+export function curryWithPlaceholder(fn) {
+    return function curried(...args) {
+        if (args.length >= fn.length && !args.slice(0, fn.length).includes(curryWithPlaceholder.placeholder)) {
+            return fn.apply(this, args);
+        }
+        return function (...newArgs) {
+            args = args.map(arg => arg === curryWithPlaceholder.placeholder && newArgs.length ? newArgs.shift() : arg);
+            return curried.call(this, ...args, ...newArgs);
+        }
+    }
+}
+
+curryWithPlaceholder.placeholder = Symbol();
