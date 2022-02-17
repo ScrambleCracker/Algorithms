438dfefd9a894bf2a80e28dff075034e911d6215
Oleh Horitsyn <oleg.goritsyn@gmail.com>
2022-02-17 14:39:42 +0200

    add throttle with options implementation

--- Frontend/throttle.js
+++ Frontend/throttle.js
@@ -16,12 +16,39 @@ export function throttle(func, wait) {
     };
 
     return function (...args) {
+        if (!timer) {
+            func(...args);
+            startCooling();
+        } else {
+            lastCall = func.bind(this, ...args);
+        }
+    };
+}
+
+export function throttleWithOptions(func, wait, option = {leading: true, trailing: true}) {
+    let lastCall = null, timer = null;
+
+    const check = () => {
+        timer = null;
+
+        if (lastCall && option.trailing) {
+            lastCall();
+            lastCall = null;
+            startCooling();
+        }
+    };
+
+    const startCooling = () => {
+        timer = setTimeout(check, wait);
+    };
+
+    return function (...args) {
         if (timer) {
             lastCall = func.bind(this, ...args);
             return;
+        } if (option.leading) {
+            func.apply(this, args);
         }
-
-        func(...args);
         startCooling();
     };
 }
